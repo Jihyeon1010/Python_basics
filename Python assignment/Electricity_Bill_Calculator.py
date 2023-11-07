4eb335b7779cd25a1e13b396fd5cf9663bdf1bc5
Jihyeon Park <146726231+Jihyeon1010@users.noreply.github.com>
2023-11-07 11:42:34 +0000

    Update Electricity_Bill_Calculator.py

--- Python assignment/Electricity_Bill_Calculator.py
+++ Python assignment/Electricity_Bill_Calculator.py
@@ -1,18 +1,29 @@
-customer_id = input("Enter Customer ID: ")
-customer_name = input("Enter Customer Name: ")
-unit_consumed = float(input("Enter the units consumed: "))
+# Input customer information
+customer_id = input("Enter customer ID: ")
+customer_name = input("Enter customer name: ")
+units_consumed = float(input("Enter units consumed: "))
 
-if unit_consumed < 100:
-    total_amount = 100  
-elif unit_consumed <= 400:
-    total_amount = unit_consumed * 1.5  
+# Define the minimum bill amount
+minimum_bill = 100.0
+
+# Calculate the total amount to pay
+if units_consumed <= 100:
+    total_amount = minimum_bill
+elif units_consumed <= 200:
+    total_amount = minimum_bill + (units_consumed - 100) * 1.5
+elif units_consumed <= 300:
+    total_amount = minimum_bill + 100 * 1.5 + (units_consumed - 200) * 2.0
 else:
-    total_amount = unit_consumed * 1.5  
-    surcharge = 0.15 * total_amount  
+    total_amount = minimum_bill + 100 * 1.5 + 100 * 2.0 + (units_consumed - 300) * 2.5
+
+# Apply a 15% surcharge if the total amount exceeds £400
+if total_amount > 400:
+    surcharge = total_amount * 0.15
     total_amount += surcharge
 
-print("Electricity Bill")
+# Display the electricity bill
+print("\nElectricity Bill")
 print("Customer ID:", customer_id)
 print("Customer Name:", customer_name)
-print("Units Consumed:", unit_consumed)
-print("Total Amount to Pay: £", total_amount)
+print("Units Consumed:", units_consumed, "units")
+print("Total Amount to Pay: £", round(total_amount, 2))
